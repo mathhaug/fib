929e868d8c1216109d92454a46a92826e1a7d561
Mathias Haugen <haugen1997@gmail.com>
2023-05-04 06:33:53 +0200

    h

---
+++ fib/fib_test.go
@@ -0,0 +1,14 @@
+package fib
+
+import "testing"
+
+func TestFib(t *testing.T) {
+	// Her går testen
+
+	input := 0
+	got := Fib(input)
+	want := 0
+	if got != want {
+		t.Errorf("Fib(%d): want %d, got %d", input, want, got)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mathhaug/fib.git
+
+go 1.20
