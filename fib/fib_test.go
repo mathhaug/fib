060d3d1c22a279c524bed8697dd0e94e7d1743ba
Mathias Haugen <haugen1997@gmail.com>
2023-05-04 06:45:12 +0200

    h

--- fib/fib_test.go
+++ fib/fib_test.go
@@ -11,4 +11,31 @@ func TestFib(t *testing.T) {
 	if got != want {
 		t.Errorf("Fib(%d): want %d, got %d", input, want, got)
 	}
+	input = 1 // legg merke til operatøren =
+	got = Fib(input)
+	want = 1
+	if got != want {
+		t.Errorf("Fib(%d): want %d, got %d", input, want, got)
+	}
+
+	type test struct {
+		input int
+		want  int
+	}
+
+	var tests = []test{
+		{0, 0},
+		{1, 1},
+		{2, 1},
+		{3, 2},
+		// her kan vi bare legge på nye test cases
+	}
+
+	// tc - test case
+	for _, tc := range tests {
+		got := Fib(tc.input)
+		if got != tc.want {
+			t.Errorf("Fib(%d): want %d, got %d", tc.input, tc.want, got)
+		}
+	}
 }
